a96788a641f1c4709f22be62932b8c731769a225
Geon-05 <zqrc05@gmail.com>
2025-01-10 09:41:13 +0900

    20250110

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ import mysql.connector
 def get_db_connection():
     return mysql.connector.connect(
         host="58.72.151.123",
-        user="human2",  # MySQL 사용자 이름
-        password="Passw0rd!123",  # MySQL 비밀번호
-        database="mydb"  # 데이터베이스 이름
+        user="human2",
+        password="Passw0rd!123",
+        database="mydb"
     )
